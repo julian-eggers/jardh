0f112e4e028dbdcffa27680b6e2f921eca41aa78
Julian Eggers <jeggers@avides.com>
2015-07-24 07:04:48 +0200

    setup.sh improved

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ -z ${1+x} ]; then
+        echo "append path to app-environment ( setup.sh /home/exampleuser/ )"
+        exit 1;
+fi
+
+cd $1
+
 mkdir apps_deployable
 mkdir apps_running
 mkdir backups
@@ -7,5 +14,5 @@ mkdir data
 mkdir logs
 mkdir scripts
 
-wget https://raw.githubusercontent.com/julian-eggers/jardh/master/management.sh
-mv management.sh scripts/
+cd scripts/
+wget "https://raw.github.com/julian-eggers/jardh/master/management.sh" -O "management.sh" -nv
