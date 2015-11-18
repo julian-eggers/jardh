13e344fc08e159a8cff55f7746bfb31281dc35c1
Julian Eggers <julian-eggers@users.noreply.github.com>
2015-11-18 21:07:39 +0100

    Update example.spring-boot.sh
    KillCommand removed

--- example.spring-boot.sh
+++ example.spring-boot.sh
@@ -2,17 +2,16 @@
 
 AUTO_UPDATE_ENABLED="1"
 
-JVM_PARAMETERS="-Dprofile=production -Djava.awt.headless=true"
-CONSOLE_LOG_FILE="/root/logs/web.output.log"
+JVM_PARAMETERS="-Djava.awt.headless=true"
+CONSOLE_LOG_FILE="/root/logs/app.output.log"
 
 APP_PID="`wget -qO- --header='Accept: text/html' http://localhost:17801/env/PID`"
 APP_SHUTDOWN_COMMAND="curl -X POST --connect-timeout 10 http://localhost:17801/shutdown"
 APP_SHUTDOWN_TIMEOUT=10
-APP_KILL_COMMAND="kill -9 $APP_PID"
 
-JAR_BACKUP_FOLDER="/root/backups/jars/web/"
-RUNNING_JAR="/root/apps_running/web.jar"
-DEPLOYABLE_JAR="/root/apps_deployable/web.jar"
+JAR_BACKUP_FOLDER="/root/backups/jars/app/"
+RUNNING_JAR="/root/apps_running/app.jar"
+DEPLOYABLE_JAR="/root/apps_deployable/app.jar"
 
 
 source $(dirname $(readlink -f $0))/management.sh
